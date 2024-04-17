2ceb24609049a5ad4208efdc0432df8082353b2a
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-04-17 16:48:24 +0200

    Add mock coworkings data

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 // Définition du port sur lequel l'application écoutera les requêtes
 const port = 3000
 
+const coworkingsData = require('./coworkings')
+
 // Définition des routes et des gestionnaires de requêtes pour l'application
 
 // Route racine de l'application
@@ -22,6 +24,26 @@ app.get('/api/user', (req, res) => {
     res.send('Hello comment !')  // Réponse renvoyée au client lorsqu'une requête GET est effectuée sur la route '/api/comment'
 })
 
+// Route pour l'API coworkings
+app.get('/api/coworkings', (req, res) => {
+    res.send('Hello toi !')  
+})
+
+// Définition d'une route avec un paramètre d'URL
+// Le paramètre ":id" dans l'URL permet de spécifier un identifiant unique pour récupérer des données spécifiques.
+app.get('/api/coworkings/:id', (req, res) => {
+    console.log(req)
+    // Renvoi d'une réponse au client avec le numéro d'identifiant récupéré depuis les paramètres de l'URL
+    res.send(`Hello coworkers n°${req.params.id} !`)
+})
+
+app.get('/api/user/:id', (req, res) => {
+    res.send(`Hello user n°${req.params.id} !`)
+})
+
+app.get('/api/comment/:id', (req, res) => {
+    res.send(`Hello comment n°${req.params.id} !`)
+})
 
 // Configuration de l'application pour écouter les requêtes sur le port spécifié
 app.listen(port, () => {
