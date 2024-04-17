ac76c3f81787cf25719e5efe960cdd79dadcc8f6
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-04-17 16:24:16 +0200

    add comment

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+// Importation du module Express
 const express = require('express')
+
+// Initialisation de l'application Express
 const app = express()
+// Définition du port sur lequel l'application écoutera les requêtes
 const port = 3000
 
+// Définition des routes et des gestionnaires de requêtes pour l'application
+
+// Route racine de l'application
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!')  // Réponse renvoyée au client lorsqu'une requête GET est effectuée sur la route '/'
 })
 
+// Route pour l'API utilisateur
+app.get('/api/user', (req, res) => {
+    res.send('Hello Users !')  // Réponse renvoyée au client lorsqu'une requête GET est effectuée sur la route '/api/user'
+  })
+// Route pour l'API commentaire
+  app.get('/api/comment', (req, res) => {
+    res.send('Hello comment !')  // Réponse renvoyée au client lorsqu'une requête GET est effectuée sur la route '/api/comment'
+})
+
+
+// Configuration de l'application pour écouter les requêtes sur le port spécifié
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)  // Message de console indiquant que l'application écoute sur le port spécifié
 })
