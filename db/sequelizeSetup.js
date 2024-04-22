804c4559a8650c6653a2cb70d218ef5af459fcd0
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-04-22 17:22:51 +0200

    END DAY 4

--- db/sequelizeSetup.js
+++ db/sequelizeSetup.js
@@ -4,14 +4,17 @@ const CoworkingModel = require('../models/coworkingModel')
 const mockCoworkings = require('../coworkings')
 
 // Option: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('bdx_coworkings', 'root', '', {
+const sequelize = new Sequelize('bx_coworkings', 'root', '', {
     host: 'localhost',
     dialect: 'mariadb',
     logging: false
 });
 
-const Coworking = CoworkingModel(sequelize)
+const Coworking = CoworkingModel(sequelize);
 
+// (() => {
+
+// })()
 sequelize.sync({ force: true })
     .then(() => {
         mockCoworkings.forEach(coworking => {
